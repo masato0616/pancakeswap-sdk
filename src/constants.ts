c217fb6fddb3fd6949d6fd43269bce2fd2ad6cfb
田中雅智 <fang.of.jadeite.gae@gmail.com>
2021-04-25 01:24:23 +0900

    変更

--- src/constants.ts
+++ src/constants.ts
@@ -21,7 +21,7 @@ export enum Rounding {
 
 export const FACTORY_ADDRESS = '0x22b7159b889acE3FE3cc7C4Cc20D85513de4ab0A'
 
-export const INIT_CODE_HASH = '0xd0d4c4cd0848c93cb4fd1f498d7013ee6bfb25783ea21593d5834f5d250ece66'
+export const INIT_CODE_HASH = '0x23e65c95865adff88333195bf94317521c15376ab98cd9872eaf19f50ea80495'
 
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
 
