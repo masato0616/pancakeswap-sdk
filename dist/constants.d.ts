fd3bf6fa8e79861cfbfa03ec1c2ff9df2b960ccc
田中雅智 <fang.of.jadeite.gae@gmail.com>
2021-05-31 00:57:17 +0900

    変更

--- dist/constants.d.ts
+++ dist/constants.d.ts
@@ -13,7 +13,7 @@ export declare enum Rounding {
     ROUND_HALF_UP = 1,
     ROUND_UP = 2
 }
-export declare const FACTORY_ADDRESS = "0x8cEd4b6026157e4c7cCe52bDA7BcCEF7B715Be79";
+export declare const FACTORY_ADDRESS = "0x6D46fB88466E371b6c91be4c263430904eE310C4";
 export declare const INIT_CODE_HASH = "0x23e65c95865adff88333195bf94317521c15376ab98cd9872eaf19f50ea80495";
 export declare const MINIMUM_LIQUIDITY: JSBI;
 export declare const ZERO: JSBI;
